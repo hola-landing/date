8e55dfb611a334818c77a228c59ada8bfaeac052
hola-landing <72271870+hola-landing@users.noreply.github.com>
2020-10-02 14:06:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const hideNo = () => {
     no.style.top = randomy + "px";
     if(counter == 3){
         no.removeEventListener("mouseenter", hideNo);
-        no.style.position = "relative";
     }
 }
 
